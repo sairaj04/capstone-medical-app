1c200e51be57ce254522609dc07d3cee44e9f475
sairaj04 <sairaj04480@gmail.com>
2023-12-07 02:33:58 -0500

    ERROR on Step 14, this is for help from coursera team

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from './Components/Navbar/Navbar';
 function App() {
-  return (
-    <> </>
-  );
+
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                </Routes>
+
+            </BrowserRouter>
+
+        </div>
+    );
 }
 
 export default App;
