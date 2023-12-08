a78268d7d8e61bd68f5e07eeaea4ce650d5a3b65
sairaj04 <sairaj04480@gmail.com>
2023-12-08 02:17:17 -0500

    Pushing to Checking to ensure step 16 & 17 done properly or not from coursera

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Landing_Page from './Components/Landing_Page/LandingPage';
 import Navbar from './Components/Navbar/Navbar';
+import Layout from './Components/Landing_Page/LandingPage';
+
 function App() {
 
     return (
@@ -10,8 +12,8 @@ function App() {
             <BrowserRouter>
                 <Navbar />
                 <Routes>
+                    <Route path='/' element={<Landing_Page/>}></Route>
                 </Routes>
-
             </BrowserRouter>
 
         </div>
