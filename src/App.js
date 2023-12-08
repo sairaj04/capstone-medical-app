11681cc515b05520f728695a0d8a73363f4813af
sairaj04 <sairaj04480@gmail.com>
2023-12-08 08:30:41 -0500

    Week 3 lab 2 Ex:2 Sign Up & Login Components added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React, { useEffect } from 'react';
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing_Page from './Components/Landing_Page/LandingPage';
 import Navbar from './Components/Navbar/Navbar';
-import Layout from './Components/Landing_Page/LandingPage';
+import LandingPage from './Components/Landing_Page/LandingPage';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
+import Login from './Components/Login/Login';
 
 function App() {
-
     return (
         <div className="App">
             <BrowserRouter>
                 <Navbar />
                 <Routes>
-                    <Route path='/' element={<Landing_Page/>}></Route>
+                    <Route path="/LandingPage" element={<LandingPage />} />
+                    <Route path="/Sign_Up" element={<Sign_Up />} />
+                    <Route path="/Login" element={<Login />} />
                 </Routes>
             </BrowserRouter>
-
         </div>
     );
 }
-
 export default App;
