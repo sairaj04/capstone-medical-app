6b818532f43372f348f4b2b1c5ac5cdf339387d6
sairaj04 <sairaj04480@gmail.com>
2023-12-08 03:06:41 -0500

    Previous commit is right and Successfully completed Step 16 & 17 modified LandingPage.js Healthcare Description

--- src/Components/Landing_Page/LandingPage.js
+++ src/Components/Landing_Page/LandingPage.js
@@ -3,36 +3,36 @@ import { Link } from "react-router-dom";
 import "./LandingPage.css";
 
 const Landing_Page = () => {
-  return (
-    <section className="hero-section">
-      <div>
-        <div data-aos="fade-up" className="flex-hero">
-            
-            <h1>
-              Your Health<br/>
+    return (
+        <section className="hero-section">
+            <div>
+                <div data-aos="fade-up" className="flex-hero">
 
-              <span className="text-gradient">
-                
-                Our Responsibility
-              </span>
-            </h1>
-              <div class="blob-cont">
-                  <div class="blue blob"></div>
-              </div>
-              <div class="blob-cont">
-                  <div class="blue1 blob"></div>
-              </div>
-            <h4>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque at quae ducimus. Suscipit omnis quibusdam non cum rem voluptatem!
-            </h4>
-            <a href="#services">
-              <button class="button">Get Started</button>
-            </a>
-        </div>
+                    <h1>
+                        Your Health<br />
 
-      </div>
-    </section>
-  );
+                        <span className="text-gradient">
+
+                            Our Responsibility
+                        </span>
+                    </h1>
+                    <div class="blob-cont">
+                        <div class="blue blob"></div>
+                    </div>
+                    <div class="blob-cont">
+                        <div class="blue1 blob"></div>
+                    </div>
+                    <h4>
+                        When nobility of purpose is powered by knowledge and technology, there are no limits to excellence. At StayHealthy Healthcare, we strive for excellence, every moment and every day - to bring great healthcare within reach for millions of people.
+                    </h4>
+                    <a href="#services">
+                        <button class="button">Get Started</button>
+                    </a>
+                </div>
+
+            </div>
+        </section>
+    );
 };
 
 export default Landing_Page;
