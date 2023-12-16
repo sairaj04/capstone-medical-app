8757e51810f5dbc3a65e5ce0abba5c8d8191133c
sairaj04 <sairaj04480@gmail.com>
2023-12-16 01:32:57 -0500

    Week 3 Lab 4 Ex:3 Added AppointmentForm component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
 import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
-import DoctorCard from "./Components/DoctorCard/DoctorCard"
 
 function App() {
     return (
@@ -18,9 +17,7 @@ function App() {
                     <Route path="/Sign_Up" element={<Sign_Up />} />
                     <Route path="/Login" element={<Login />} />
                     <Route path="/instant-consultation" element={<InstantConsultation />} />
-                    <Route path='/finddoctor' element={<FindDoctorSearch/>}/>
-                    <Route path='/doctor-card' element={<DoctorCard/>}/>
-                    
+                    <Route path='/finddoctor' element={<FindDoctorSearch/>}/>                    
                 </Routes>
             </BrowserRouter>
         </div>
